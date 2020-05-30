c1e3a801c080a1fc0eb6ef4bd543333caf355a13
Dmitry Ivanov <wayper.plenitude@gmail.com>
2020-05-30 12:39:08 +0300

    Hook: USE STATE

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import TodoList from './TodoList'
 
-export default class App extends Component {
-  state = {
-    todos: [
-      {id: 1, title: 'First todo', completed: false},
-      {id: 2, title: 'Second todo', completed: true},
-    ]
+export default function App() {
+  // state = {
+  //   todos: [
+  //     {id: 1, title: 'First todo', completed: false},
+  //     {id: 2, title: 'Second todo', completed: true},
+  //   ]
+  // }
+  const [todos, setTodos] = useState([
+    {id: 1, title: 'First todo', completed: false},
+    {id: 2, title: 'Second todo', completed: true},
+  ])
+  const [todoTitle, setTodoTitle] = useState('')
+
+  const addTodo = e => {
+    if(e.key === 'Enter') {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false,
+        }
+      ])
+      setTodoTitle('')
+    }
   }
 
-  render() {
     return (
       <div className="container">
         <h1>Todo app</h1>
 
           <div className="input-field">
-            <input type="text" />
+            <input
+              type="text"
+              value={todoTitle}
+              onChange={e => setTodoTitle(e.target.value)}
+              onKeyPress={addTodo}
+            />
             <label>Todo name</label>
           </div>
 
-          <TodoList todos={this.state.todos} />
+          <TodoList todos={todos} />
       </div>
     );
-  }
 }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TodoItem({title, id, completed}) {
+  const [checked, setChecked] = useState(completed)
+
+  const cls = ['todo']
+
+  if (checked) {
+    cls.push('completed')
+  }
   return (
-    <li className="todo">
+    <li className={cls.join(' ')}>
       <label>
         <input
           type="checkbox"
           defaultChecked={false}
+          onChange={() => setChecked(!checked)}
         />
         <span>{title}</span>
 
